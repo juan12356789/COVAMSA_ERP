ef647c390ef8451de3fd49dc3098ed9a69809e9d
juan12356789 <up170132@alumnos.upa.edu.mx>
2020-02-26 11:42:07 -0600

    subir

--- src/routes/ventas/ventas.js
+++ src/routes/ventas/ventas.js
@@ -42,27 +42,28 @@ const storage=multer.diskStorage({
     let data =  JSON.stringify(req.body).toUpperCase();
     let {ORDEN,NUMEROCOTIZACION,NOMBRE,IMPORTE,OBSERVACIONES,RUTA} = JSON.parse(data);
     if(NOMBRE  === undefined)  return; 
-    const id  =  await pool.query('SELECT idcliente  FROM clientes where nombre   = ?', [NOMBRE] ); 
-    const idEmpleado  = await  pool.query('SELECT id_empleados from empleados WHERE idacceso  = ?',[req.user.idacceso]);    
-    
-    
+    const id  =  await pool.query('SELECT idcliente  FROM clientes where nombre   = ?', [NOMBRE] );
+    console.log(req.user);
+     
+    const idEmpleado  = await  pool.query('SELECT id_empleados from empleados WHERE idacceso  = ?',[req.user[0].idacceso]);    
+    console.log(idEmpleado);
+  
     let  pedido  = {
         id_pedido:null, 
-        idcliente: id[0].idcliente,
         id_empleado : idEmpleado[0].id_empleados, //cambiar cuand0 haga el login
+        idcliente: id[0].idcliente,
         orden_de_compra: ORDEN,
         ruta: RUTA,
         estatus: 1 ,
-        ruta_pdf_orden_compra: req.files[0].filename,
-        cotizacion: NUMEROCOTIZACION,
-        ruta_pdf_cotizacion: req.files[1].filename
+        ruta_pdf_orden_compra: req.files[0].filename
         }; 
-        console.log(pedido);
         
-
-   if( /^[0-9a-zA-Z]+$/.test(ORDEN) || /^[0-9a-zA-Z]+$/.test(NUMEROCOTIZACION)) {
-         await pool.query("INSERT INTO  pedidos  set ?", [pedido]);
-         const idPedido  = await pool.query('select id_pedido from pedidos where orden_de_compra = ? and cotizacion = ?',[ORDEN,NUMEROCOTIZACION]);
+        
+        if( /^[0-9a-zA-Z]+$/.test(ORDEN) || /^[0-9a-zA-Z]+$/.test(NUMEROCOTIZACION)) {
+          console.log('hola');
+          await pool.query("INSERT INTO  pedidos  set ?", [pedido]);
+          
+         /* const idPedido  = await pool.query('select id_pedido from pedidos where orden_de_compra = ? and cotizacion = ?',[ORDEN,NUMEROCOTIZACION]);
 
          let infoPedido  = {
           id_informacion_pedido: null, 
@@ -71,8 +72,8 @@ const storage=multer.diskStorage({
           importe : IMPORTE,
           fecha_pedido:format(new Date(), 'YYYY-MM-DD'),
           estatus: 1
-        }
-        await pool.query("INSERT INTO informacion_pedido set ?",[infoPedido]);
+        }*/
+       // await pool.query("INSERT INTO informacion_pedido set ?",[infoPedido]);
         req.flash('success','Pedido Guardado' ); 
         res.redirect('/ventas')  ; 
       }
