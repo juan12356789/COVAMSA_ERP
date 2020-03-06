7568583748f427880d66f3e80c8f86b7f6483002
rosalizethestrada <up170043@alumnos.upa.edu.mx>
2020-03-06 12:22:32 -0600

    subir 2

--- src/public/JS/validaciones.js
+++ src/public/JS/validaciones.js
@@ -1,55 +1,81 @@
 $('#clientesPorBusqueda').hide();
+// busca los  clientes 
+$("#clientes").click(function(e) {
 
-$("#prueba").click(function(e) {
     $('.col-sm-8').val('');
     $("#ocultar").show();
     $("#clientesPorBusqueda").hide();
     $('#inputBusqueda').keyup(function(e) {
+
         if ($('#inputBusqueda').val()) {
             $.post("/ventas", { busqueda: $("#inputBusqueda").val() }, function(data) {
                 $("#ocultar").hide();
+
                 let tabla = `<table class="table table-bordered-responsive-sm">
-              <thead class="thead-light">
+             <thead class="thead-light">f
          <tr>
-           <th scope="col">NOMBRE</th>
-           <th scope="col">ID</th>
-           <th scope="col">BUSCAR</th>
-         </tr>
-       </thead>`;
+             <th scope="col">NOMBRE</th>
+             <th scope="col">ID</th>
+             <th scope="col">BUSCAR</th>
+          </tr>
+          </thead>`;
+                console.log(data);
+
                 data.forEach(data => {
                     tabla += `
-                <tr>
-                  <td>${data.nombre}</td>
-                  <td>${data.numero_interno}</td>
-                  <td><button type="button" onclick="clientes('${data.nombre}')" class="btn btn-success" data-dismiss="modal">Seleccionar</button></td>
-                </tr> 
-              `;
+                 <tr>
+                   <td>${data.nombre}</td>
+                   <td>${data.numero_interno}</td>
+                 <td> <button type="button" onclick="clientes('${data.nombre}')" class="btn btn-success" data-dismiss="modal">Seleccionar</button></td>
+                 </tr> 
+               `;
+
                 });
                 tabla += "</table>";
                 $("#clientesPorBusqueda").show();
                 document.getElementById('clientesPorBusqueda').innerHTML = tabla;
-              
-              });
 
-  let clientes = (nombre)=>{
-    let mandar  = `
-      <div class="form-group row justify-content-center ">
-      <label for="" class="col-sm-2 col-form-label"></label>
-      <div class="col-sm-5">
-        <input type="text"  class="form-control valid border border-secondary" value="${nombre}" name="nombre">
-      </div>
-      </div>
-      </div>
-   `;
-    document.getElementById('inputCliente').innerHTML =  mandar;
-    pagos(nombre); 
-  }; 
+            });
 
-  let pagos = (cliente)=>{
-    $.post("/ventas/pagos",{cliente:cliente},function (data) {
-        console.log(data);
-      }
+        }
+
+    });
+
+});
+
+//manda el cliente a la tabla 
+let clientes = (nombre) => {
+    console.log("ola ");
+    let mandar = `
+         <div class="form-group row justify-content-center ">
+         <label for="" class="col-sm-2 col-form-label"></label>
+         <div class="col-sm-5">
+           <input type="text"  class="form-control valid border border-secondary" value="${nombre}" name="nombre">
+         </div>
+         </div>
+         </div>
+      `;
+    document.getElementById('inputCliente').innerHTML = mandar;
+    pagos(nombre);
+
+};
+
+//Trae los tipos de pago 
+let pagos = (cliente) => {
+    console.log("ola ");
+
+    $.post("/ventas/pagos", { cliente: cliente }, function(data) {
+
+            console.log(data);
+            //mandar al fronttend
+            // let ver =
+            // document.getElementById(data).innerHTML = cliente;
+            // pagos(cliente);
+
+
+
+
+        }
+
     );
-
-  };
-
+};
