081c56f24459253253ce6f2c54f3f1f5b7c268ab
juan12356789 <up170132@alumnos.upa.edu.mx>
2020-03-09 09:29:28 -0600

    subir

--- src/public/JS/validaciones.js
+++ src/public/JS/validaciones.js
@@ -1,47 +1,62 @@
- $('#clientesPorBusqueda').hide(); // busca los  clientes  $("#clientes").click(function(e) {
- $('.col-sm-8').val('');
- $("#ocultar").show();
- $("#clientesPorBusqueda").hide();
+let classText =()=>{
+  $(".orden_compra").addClass("col-sm-2 col-form-label");
+  $(".numero_pedido").addClass("col-sm-2 col-form-label");
+  $(".comprobante_pago").addClass("col-sm-2 col-form-label");
+  $(".observaciones").addClass("col-sm-2 col-form-label"); 
+ 
+};  
+classText(); 
 
- $('#inputBusqueda').keyup(function(e) {
-     if ($('#inputBusqueda').val()) {
-         $.post("/ventas", { busqueda: $("#inputBusqueda").val() }, function(data) {
-             $("#ocultar").hide();
+$("#clientes").click(function (e) { 
 
+  $('#clientesPorBusqueda').hide();
+  $("#inputCliente").hide();
+  $('.col-sm-8').val('');
+  $("#ocultar").show();
+  $("#clientesPorBusqueda").hide();
+  $('#inputBusqueda').keyup(function(e) {
 
-             let tabla = `<table class="table table-bordered-responsive-sm">
-               <thead class="thead-light">
-          <tr>
-            <th scope="col">NOMBRE</th>
-            <th scope="col">ID</th>
-            <th scope="col">BUSCAR</th>
-          </tr>
-        </thead>`;
-             
-             data.forEach(data => {
-                 tabla += `
-                 <tr>
-                   <td>${data.nombre}</td>
-                   <td>${data.numero_interno}</td>
-                 <td> <button type="button" onclick="cliente('${data.nombre}')" class="btn btn-success" data-dismiss="modal">Seleccionar</button></td>
-                 </tr> 
-               `
-             });
-             tabla += "</table>";
-             $("#clientesPorBusqueda").show();
-             document.getElementById('clientesPorBusqueda').innerHTML = tabla;
+    if ($('#inputBusqueda').val()) {
+        $.post("/ventas", { busqueda: $("#inputBusqueda").val() }, function(data) {
+            $("#ocultar").hide();
 
 
-         });
+            let tabla = `<table class="table table-bordered-responsive-sm">
+              <thead class="thead-light">
+         <tr>
+           <th scope="col">NOMBRE</th>
+           <th scope="col">ID</th>
+           <th scope="col">BUSCAR</th>
+         </tr>
+       </thead>`;
+            
+            data.forEach(data => {
+                tabla += `
+                <tr>
+                  <td>${data.nombre}</td>
+                  <td>${data.numero_interno}</td>
+                <td> <button type="button" onclick="cliente('${data.nombre}')" class="btn btn-success" data-dismiss="modal">Seleccionar</button></td>
+                </tr> 
+              `
+            });
+            tabla += "</table>";
+            $("#clientesPorBusqueda").show();
+            document.getElementById('clientesPorBusqueda').innerHTML = tabla;
 
-     }
 
- });
+        });
+
+    }
+
+});
+  
+});
+
 
  //manda el cliente a la tabla 
 
  let cliente = (nombre) => {
-
+    
 
      let mandar = `
        <div class="form-group row justify-content-center ">
@@ -53,17 +68,24 @@
        </div>
     `;
      pagos(nombre);
+     $("#inputCliente").show();
      document.getElementById('inputCliente').innerHTML = mandar;
 
  };
 
  //Trae los tipos de pago 
  let pagos = (cliente) => {
-
+    
      $.post("/ventas/pagos", { cliente: cliente }, function(data) {
-         data.forEach(data => {
-             $("#prueba").append(data.tipo_pago);
-         })
+      
+        $(".orden_compra").removeClass("col-sm-2 col-form-label text-danger");
+        $(".numero_pedido").removeClass("col-sm-2 col-form-label text-danger");
+        $(".comprobante_pago").removeClass("col-sm-2 col-form-label text-danger");
+        $(".obervaciones").removeClass("col-sm-2 col-form-label text-danger");
+      data.forEach(data => {
+          $("."+data.tipo_pago).addClass("col-sm-2 col-form-label text-danger");
+       });
      });
 
  };
+ 
