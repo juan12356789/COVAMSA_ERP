dce9fbb5ba1916b8e108bcb76b60ebb1b23f87b4
rosalizethestrada <up170043@alumnos.upa.edu.mx>
2020-04-01 09:47:26 -0600

    ort

--- src/public/JS/pedidos.js
+++ src/public/JS/pedidos.js
@@ -24,26 +24,34 @@ let sendData = (data) => {
     
     let table = '';
         ruta = ["NORTE", "SUR"];
-<<<<<<< HEAD
+        
+// <<<<<<< HEAD
         estatus = ['NUEVO'];
-=======
+// =======
         estatus = ['CAPTURADO'];
         console.log(data);
         
->>>>>>> e1a29f5084fd438115f622d8b17999011c482dc6
+// >>>>>>> e1a29f5084fd438115f622d8b17999011c482dc6
     data.forEach(data => {
         table+= `<tr>
-                  <td> <a  href="/almacen/pdf/${data.ruta_pdf_orden_compra}">${data.orden_de_compra}</a></td>
-                  <td> <a  href="/almacen/pdf/${data.ruta_pdf_pedido}">${data.num_pedido}</a></td>
-                  <td> <a  href="/almacen/pdf/${data.ruta_pdf_comprobante_pago}">${data.comprobante_pago}</a></td>
+                 
+                  <td> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNGhpmNOG1joyk_s0v1KH229zGd6CpZ0axtXRT6c6pqW4FlB2b&usqp=CAU" height="15">
+                  <a  href="/almacen/pdf/${data.ruta_pdf_orden_compra}">${data.orden_de_compra}</a></td>
+
+                  <td> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNGhpmNOG1joyk_s0v1KH229zGd6CpZ0axtXRT6c6pqW4FlB2b&usqp=CAU" height="15">
+                  <a  href="/almacen/pdf/${data.ruta_pdf_pedido}">${data.num_pedido}</a></td>
+
+                  <td> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSNGhpmNOG1joyk_s0v1KH229zGd6CpZ0axtXRT6c6pqW4FlB2b&usqp=CAU" height="15">
+                  <a  href="/almacen/pdf/${data.ruta_pdf_comprobante_pago}">${data.comprobante_pago}</a></td>
+
                   <td>${ruta[data.ruta - 1]}</td>
                   <td>${data.importe}</td> 
-                  <td>${estatus[data.estatus - 1]}</td> 
+                  <td style="background-color:#DF3A01">${estatus[data.estatus - 1]}</td>
                   <td>${data.observacion}</td>
                   <td>${data.fecha_inicial}</td>
                 </tr>`; 
-        
-    
     });
     document.getElementById('pedidos').innerHTML = table;
 }
+
+let table
