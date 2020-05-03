01a3012fe66b1e8eea60d64134c9505add088b2b
rosalizethestrada <up170043@alumnos.upa.edu.mx>
2020-05-03 17:24:53 -0500

    Login | Bug
    Si no pones tu password de acuerdo al pattern el input se pone en rojo.
    <Debe tener al menos una mayúscula, una minúscula y un dígito">

--- src/public/JS/almacen/pedidos.js
+++ src/public/JS/almacen/pedidos.js
@@ -9,7 +9,7 @@ let pedidos = (data) => {
             url: "/almacen/pedidos",
             success: function(response) {
 
-                if (typeof(data) == "string" && data.length < 20) alert(`EL PEDIDO CON EL CÓDIGO ${data} HA SIDO CANCELAD`);
+                if (typeof(data) == "string" && data.length < 20) alert(`EL PEDIDO CON EL CÓDIGO ${data} HA SIDO CANCELADO`);
 
                 sendData(response);
 
@@ -51,7 +51,7 @@ let sendData = (data) => {
                   <td><a  href="/almacen/pdf/${data.ruta_pdf_comprobante_pago}">${data.comprobante_pago}</a></td>
                 
                   <td  style="background-color:${data.ruta ==  1 ? "#DFBC92" : "#92C1DF"} " >${ruta[data.ruta - 1]}</td>
-                  <td id="number">${data.importe}</td> 
+                  <td id="userinput" >${data.importe}</td> 
                   <td style="background-color:${colores[data.estatus - 1]}">${estatus[data.estatus - 1]}</td>
                   <td>${data.observacion}</td>
                   <td>${data.fecha_inicial}</td>
@@ -59,4 +59,17 @@ let sendData = (data) => {
                 </tr>`;
     });
     document.getElementById('pedidos').innerHTML = table;
+}
+
+document.getElementById("userinput").onblur = function() {
+
+    //number-format the user input
+    this.value = parseFloat(this.value.replace(/,/g, ""))
+        .toFixed(2)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    //set the numeric value to a number input
+    document.getElementById("number").value = this.value.replace(/,/g, "")
+
 }
