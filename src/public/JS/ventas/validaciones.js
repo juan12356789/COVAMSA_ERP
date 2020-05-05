3363066823efbd4af88e68894ec9f2633a558899
rosalizethestrada <up170043@alumnos.upa.edu.mx>
2020-05-05 14:25:46 -0500

    Responsive | Alert Cancelar
    las tablas se cortaban cuando se hacia pequeña la pantalla.
    Revicion del modal en proceso en ventanasModales.js

--- src/public/JS/ventas/validaciones.js
+++ src/public/JS/ventas/validaciones.js
@@ -4,24 +4,24 @@ $("#spinner").hide();
 
 
 // $("#tusClientes").click(function(e) {
-let clickClientes  = ()=>{
+let clickClientes = () => {
 
     $('.col-sm-8').val("");
-    let words = ''; 
-  
+    let words = '';
 
-  
+
+
     clientes();
 
 };
-let inputClinete  = document.getElementById('inputBusqueda');
-$("#inputBusqueda").on('keypress', function () {
-      clientes($("#inputBusqueda").val())
-        
+let inputClinete = document.getElementById('inputBusqueda');
+$("#inputBusqueda").on('keypress', function() {
+    clientes($("#inputBusqueda").val())
+
 });
 // $("#inputBusqueda").keydown(function (e) { 
 //     console.log(inputClinete.value);
-    
+
 //     if($("#inputBusqueda").val() == "") cliente("");
 //     clientes($("#inputBusqueda").val()); 
 // });
@@ -31,7 +31,7 @@ $("#inputBusqueda").on('keypress', function () {
 
 let clientes = (words) => {
     console.log(words);
-    
+
     $.post("/ventas", { words: words }, function(data) {
         let table = '';
         // if (data.length == 0) clientes();
@@ -64,50 +64,51 @@ let cliente = (nombre) => {
 };
 
 $(document).ready(function() {
-  
-   
-    document.getElementById('button_send').innerHTML = `<button  type="submit"  class="btn btn-success btn-lg btn-block"   >Enviar</button>`; 
-   
-    dataTable =  $("#orders").DataTable({
-        "order": [[ 7, "desc" ]], 
-        "fnRowCallback": function(nRow, aData, iDisplayIndex, iDisplayIndexFull) { 
-             if(aData.estatus == 6)    $('td', nRow).css('color', 'red');  
-            
-            } ,
 
-        columns: [
-            {
+
+    document.getElementById('button_send').innerHTML = `<button  type="submit"  class="btn btn-success btn-lg btn-block"   >Enviar</button>`;
+
+    dataTable = $("#orders").DataTable({
+        "order": [
+            [7, "desc"]
+        ],
+        "fnRowCallback": function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+            if (aData.estatus == 6) $('td', nRow).css('color', 'red');
+
+        },
+
+        columns: [{
                 sortable: false,
-                "render": function ( data, type, full, meta ) {
+                "render": function(data, type, full, meta) {
                     return `<a href="/almacen/pdf/${full.ruta_pdf_orden_compra}"  style="a {color:#130705;} " >${full.orden_de_compra}</a>`;
                 }
             }, {
                 sortable: false,
-                "render": function ( data, type, full, meta ) {
+                "render": function(data, type, full, meta) {
                     return `<a href="/almacen/pdf/${full.ruta_pdf_pedido}" >${full.num_pedido}</a>`;
                 }
-            },{
+            }, {
                 sortable: false,
-                "render": function ( data, type, full, meta ) {
+                "render": function(data, type, full, meta) {
                     return `<a href="/almacen/pdf/${full.ruta_pdf_comprobante_pago}" >${full.comprobante_pago}</a>`;
                 }
             },
-                 { data: 'ruta'},
-                 { data: 'importe'},
-                 { data: 'nombre_estatus' },
-                 { data: 'observacion' },
-                 { data: 'fecha_inicial'},{
-                   sortable:false,
-                   "render": function(data, type, full ,meta){
-                    if(full.estatus <= 3 )return `<button type="button" class="btn btn-danger" onclick="cancelOrder('${full.num_pedido}')" class="close"    ><img src="https://image.flaticon.com/icons/svg/1936/1936477.svg" height="30" alt=""></button><br>`;
-                    return ' '; 
-                   }  
-                 }
+            { data: 'ruta' },
+            { data: 'importe' },
+            { data: 'nombre_estatus' },
+            { data: 'observacion' },
+            { data: 'fecha_inicial' }, {
+                sortable: false,
+                "render": function(data, type, full, meta) {
+                    if (full.estatus <= 3) return `<button type="button" class="btn btn-danger" onclick="cancelOrder('${full.num_pedido}')" class="close"    ><img src="https://image.flaticon.com/icons/svg/1936/1936477.svg" height="30" alt=""></button><br>`;
+                    return ' ';
+                }
+            }
 
-                ]
+        ]
 
-            }); 
-            pedidos_vendedores();
+    });
+    pedidos_vendedores();
 });
 
 
@@ -118,68 +119,71 @@ let pedidos_vendedores = () => {
         url: "/ventas/pedidos_vendedor",
         success: function(response) {
             let ruta = ['NORTE', 'SUR'];
-            let estatus = ['NUEVO','EN PROCESO','PARCIAL','COMPLETO','RUTA','CANCELADO','URGENTE'];
+            let estatus = ['NUEVO', 'EN PROCESO', 'PARCIAL', 'COMPLETO', 'RUTA', 'CANCELADO', 'URGENTE'];
             response.filter(n => n.ruta = ruta[n.ruta - 1]);
             response.filter(n => n.nombre_estatus = estatus[n.estatus - 1]);
-            dataTable.rows().remove();  
+            dataTable.rows().remove();
             dataTable.rows.add(response).draw();
         }
     });
-    
+
 };
 
-let cancelOrder = ( order ) =>{
-    
-    reson_to_cancel( order ); 
-    let inputReason = document.getElementById('motivo_cancelacion'); 
-    $("#aceptar").click(function (e) {
-        if( inputReason.value == ''  || inputReason.value.length < 30 )  return  alert('Ingrese la razón de cancelación  con un mínimo de 30 caracteres')  ; 
-           $.post("/ventas/cancel", {data : order ,  reason : inputReason.value }, function (data) {
-        
-                   pedidos_vendedores(); 
-                   pedidos(order);   
-                   $('#Ventana_Modal_order').modal('hide'); 
-                   
-               }
-           );        
-    });  
-    
-}; 
+let cancelOrder = (order) => {
+
+    reson_to_cancel(order);
+    let inputReason = document.getElementById('motivo_cancelacion');
+    $("#aceptar").click(function(e) {
+        if (inputReason.value == '' || inputReason.value.length < 30) return alert('Ingrese la razón de cancelación  con un mínimo de 30 caracteres');
+        $.post("/ventas/cancel", { data: order, reason: inputReason.value }, function(data) {
+
+            pedidos_vendedores();
+            pedidos(order);
+            $('#Ventana_Modal_order').modal('hide');
+
+        });
+    });
+
+};
 
 // socket -----------
-const  pedidos = ( data ) => {
-  console.log(data);
-  
-   socket.emit('data:pedidos', data);
+const pedidos = (data) => {
+    console.log(data);
+
+    socket.emit('data:pedidos', data);
 
 };
 
 socket.on('data:pedidos', function(data) {
-    pedidos_vendedores(); 
+    pedidos_vendedores();
 
 });
 
 // componente guardar  
-var correoPrioridad  = document.getElementById('prioridad'); 
-$("#prioridad").click(function (e) { 
+var correoPrioridad = document.getElementById('prioridad');
+$("#prioridad").click(function(e) {
 
-    if(correoPrioridad.value == 0 ) {
-        document.getElementById('button_send').innerHTML = `<button  type="submit"  class="btn btn-success btn-lg btn-block"   >Enviar</button>`; 
-    }else{
-        document.getElementById('button_send').innerHTML = `<button   type="button"  class="btn btn-success btn-lg btn-block"  onclick="order_priority()" >Enviar</button>`; 
+    if (correoPrioridad.value == 0) {
+        document.getElementById('button_send').innerHTML = `<button  type="submit"  class="btn btn-success btn-lg btn-block"   >Enviar</button>`;
+    } else {
+        document.getElementById('button_send').innerHTML = `<button   type="button"  class="btn btn-success btn-lg btn-block"  onclick="order_priority()" >Enviar</button>`;
     }
 
 });
 
- $(function () {  
-    $("#imgct").submit(function (e) { 
-        $('#Ventana_Modal').modal('hide'); 
+$(function() {
+    $("#imgct").submit(function(e) {
+        $('#Ventana_Modal').modal('hide');
         e.preventDefault();
         var formData = new FormData(document.getElementById("imgct"));
-   
+
         formData.append("dato", "valor");
 
-        $.ajax({ url: "/ventas/add", type: "POST", dataType: "html", data: formData,
+        $.ajax({
+            url: "/ventas/add",
+            type: "POST",
+            dataType: "html",
+            data: formData,
             beforeSend: function() {
                 $("#spinner").show(); // Le quito la clase que oculta mi animación 
             },
@@ -191,7 +195,7 @@ $("#prioridad").click(function (e) {
                 } else {
                     pedidos(response);
                     pedidos_vendedores();
-                    alert('El pedido ha sigo guardado con éxito '); 
+                    alert('El pedido ha sigo guardado con éxito ');
                     $("#spinner").hide();
                     $('#imgct').trigger("reset");
                     cliente(' ');
